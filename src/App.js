f15bd2edce49a56270cc5a962a0dcb7c87b5636d
Tisha Yadav <tyadhuvanshi1408@gmail.com>
2024-04-28 00:59:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
+import explorer from './data/foldersData';
+import Folder from "./components/Folder";
+import useTraverseTree from "./hooks/use-traverse-tree";
 
 function App() {
-  return (
+  const [explorerData, setExplorerData] = useState(explorer);
+
+  const {insertNode} = useTraverseTree();
+  const handleInsertNode = (folderId, item, isFolder) => {
+    const finalTree = insertNode(explorerData, folderId, item, isFolder);
+    setExplorerData(finalTree);
+  };
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Folder handleInsertNode={handleInsertNode} explorer={explorerData}/>
     </div>
   );
 }
